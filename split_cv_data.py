213facb38835e6725fc93c364e4404bc2b87582e
andreeadeac22 <andreeadeac22@gmail.com>
2019-09-16 00:07:40 -0400

    Splitting decagon data for cross-validation

---
+++ split_cv_data.py
@@ -0,0 +1,117 @@
+"""
+	Split data for cross-validation and write to file.
+"""
+import argparse
+import numpy as np
+import csv
+import random
+
+import ujson as json
+from tqdm import tqdm
+
+def read_drug_structure(drug_feat_idx_jsonl):
+	with open(drug_feat_idx_jsonl) as f:
+		drugs = [l.split('\t') for l in f]
+		drugs = {idx: json.loads(mol) for idx, mol in tqdm(drugs)}
+	return drugs
+
+
+def prepare_decagon_cv_files(opt):
+	def read_ddi_instances(ddi_csv, threshold=498, use_small_dataset=False):
+		# Building side-effect dictionary and
+		# keeping only those which appear more than threshold (498) times.
+		side_effects = {}
+		with open(ddi_csv) as csvfile:
+			drug_reader = csv.reader(csvfile)
+			for i, row in enumerate(drug_reader):
+				if i > 0:
+					did1, did2, sid, *_ = row
+					assert did1 != did2
+					if sid not in side_effects:
+						side_effects[sid] = []
+					side_effects[sid] += [(did1, did2)]
+
+		side_effects = {se: ddis for se, ddis in side_effects.items() if len(ddis) >= threshold}
+		if use_small_dataset:  # just for debugging
+			side_effects = {se: ddis for se, ddis in
+			                sorted(side_effects.items(), key=lambda x: len(x[1]), reverse=True)[:20]}
+		print('Total types of polypharmacy side effects =', len(side_effects))
+		side_effect_idx_dict = {sid: idx for idx, sid in enumerate(side_effects)}
+		return side_effects, side_effect_idx_dict
+
+	def prepare_dataset(se_dps_dict, drug_structure_dict, n_fold=10):
+		drug_idx_list = list(drug_structure_dict.keys())
+		pos_datasets = {}
+		neg_datasets = {}
+
+		for i, se in enumerate(tqdm(se_dps_dict)):
+			pos_se_ddp = list(se_dps_dict[se])  # copy
+			neg_se_ddp = create_negative_instances(
+				drug_idx_list, set(pos_se_ddp), size=len(pos_se_ddp))
+
+			random.shuffle(pos_se_ddp)
+			random.shuffle(neg_se_ddp)
+			pos_datasets[se] = pos_se_ddp
+			neg_datasets[se] = neg_se_ddp
+
+		return pos_datasets, neg_datasets
+
+	def create_negative_instances(drug_idx_list, positive_set, size=None):
+		''' For test and validation set'''
+		negative_set = set()
+		if not size:
+			size = len(positive_set)
+
+		while len(negative_set) < size:
+			drug1, drug2 = np.random.choice(drug_idx_list, size=2, replace=False)
+			assert drug1 != drug2, 'Shall never happen.'
+
+			neg_se_ddp1 = (drug1, drug2)
+			neg_se_ddp2 = (drug2, drug1)
+
+			if neg_se_ddp1 in negative_set or neg_se_ddp2 in negative_set:
+				continue
+			if neg_se_ddp1 in positive_set or neg_se_ddp2 in positive_set:
+				continue
+
+			negative_set |= {neg_se_ddp1}
+		return list(negative_set)
+
+	opt.drug_dict = read_drug_structure(opt.drug_data)
+	opt.side_effects, opt.side_effect_idx_dict = read_ddi_instances(
+		opt.ddi_data, use_small_dataset=opt.debug)
+	opt.pos_datasets, opt.neg_datasets = prepare_dataset(opt.side_effects, opt.drug_dict)
+	opt.n_atom_type = 100
+	opt.n_bond_type = 20  # 12 in polypharmacy dataset
+	opt.n_side_effect = len(opt.side_effects)
+	return opt
+
+
+def prepare_qm9_cv_files(opt):
+	return opt
+
+
+def main():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('datasets', metavar='D', type=str.lower,
+	                    nargs='+', choices=['qm9', 'decagon'],
+	                    help='Name of dataset to download [QM9,DECAGON]')
+
+	parser.add_argument('--ddi_data', default='bio-decagon-combo.csv')
+	parser.add_argument('--drug_data', default='drug.feat.wo_h.self_loop.idx.jsonl')
+	parser.add_argument('-n_fold', default=10, type=int)
+	parser.add_argument('--debug', action='store_true')
+	opt = parser.parse_args()
+
+	if "qm9" in opt.datasets:
+		opt = prepare_qm9_cv_files(opt)
+
+	if "decagon" in opt.datasets:
+		opt = prepare_decagon_cv_files(opt)
+
+	print('Dump to file:', opt.output_npy)
+	np.save(opt.output_npy, opt)
+
+if __name__ == '__main__':
+	main()
+
--- train.py
+++ train.py
@@ -1,5 +1,83 @@
 """
 	File used to train the networks.
 """
-import torch
+import csv
+import os
+import pprint
+import time
+import random
+import logging
+import argparse
+from prettytable import PrettyTable
 
+import numpy as np
+from sklearn import metrics
+from tqdm import tqdm
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import torch.utils.data
+
+
+def main():
+	parser = argparse.ArgumentParser()
+
+	# Directory to resume training from
+	parser.add_argument('--trained_setting_pkl', default=None,
+						help='Load trained model from setting pkl')
+
+	# Directory containing precomputed training data split.
+	parser.add_argument('--data_pkl', default=None,
+						help="Fold data path, e.g. ./data/decagon/folds/")
+
+	parser.add_argument('-f', '--fold', default='1/10', type=str,
+	                    help="Which fold to run, format x/total")
+
+	# Dirs
+	parser.add_argument('--model_dir', default='./exp_trained')
+	parser.add_argument('--result_dir', default='./exp_results')
+	parser.add_argument('--setting_dir', default='./exp_settings')
+	parser.add_argument('-mm', '--memo', help='Memo for experiment', default='default')
+
+	parser.add_argument('-b', '--batch_size', type=int, default=128)
+
+	parser.add_argument('-d_h', '--d_hid', type=int, default=32)
+	parser.add_argument('-d_a', '--d_atom_feat', type=int, default=3)
+	parser.add_argument('-n_p', '--n_prop_step', type=int, default=3)
+	parser.add_argument('-n_h', '--n_attention_head', type=int, default=1)
+	parser.add_argument('-score', '--score_fn', default='trans', const='trans',
+						nargs='?', choices=['trans', 'factor'])
+
+	parser.add_argument('-dbg', '--debug', action='store_true')
+	parser.add_argument('-e', '--n_epochs', type=int, default=10000)
+	parser.add_argument('-lr', '--learning_rate', type=float, default=1e-3)
+	parser.add_argument('-l2', '--l2_lambda', type=float, default=0)
+	parser.add_argument('-drop', '--dropout', type=float, default=0.1)
+	parser.add_argument('--patience', type=int, default=32)
+	parser.add_argument('--seed', type=int, default=777)
+	parser.add_argument('-nr', '--train_neg_pos_ratio', type=int, default=1)
+	parser.add_argument('-tr', '--transR', action='store_true')
+	parser.add_argument('-th', '--transH', action='store_true')
+
+	opt = parser.parse_args()
+
+	opt.fold_i, opt.n_fold = map(int, opt.fold.split('/'))
+	assert 0 < opt.fold_i <= opt.n_fold
+
+	is_resume_training = opt.trained_setting_pkl is not None
+	if is_resume_training:
+		logging.info('Resume training from', opt.trained_setting_pkl)
+		opt = np.load(open(opt.trained_setting_pkl, 'rb')).item()
+	else:
+		opt = post_parse_args(opt)
+		setup_directories(opt)
+		# save the dataset split in setting dictionary
+		pprint.pprint(vars(opt))
+		# save the setting
+		logging.info('Related data will be saved with prefix: %s', opt.exp_prefix)
+
+
+
+if __name__ == "__main__":
+	main()

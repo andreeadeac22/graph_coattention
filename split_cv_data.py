3c1a6a9d9dfa4717335260c83ccd6332ab5b43db
andreeadeac22 <andreeadeac22@gmail.com>
2019-09-16 00:28:10 -0400

    Starting cv split for qm9

--- split_cv_data.py
+++ split_cv_data.py
@@ -9,10 +9,10 @@ import random
 import ujson as json
 from tqdm import tqdm
 
-def read_drug_structure(drug_feat_idx_jsonl):
+def read_graph_structure(drug_feat_idx_jsonl):
 	with open(drug_feat_idx_jsonl) as f:
 		drugs = [l.split('\t') for l in f]
-		drugs = {idx: json.loads(mol) for idx, mol in tqdm(drugs)}
+		drugs = {idx: json.loads(graph) for idx, graph in tqdm(drugs)}
 	return drugs
 
 
@@ -77,7 +77,8 @@ def prepare_decagon_cv_files(opt):
 			negative_set |= {neg_se_ddp1}
 		return list(negative_set)
 
-	opt.drug_dict = read_drug_structure(opt.drug_data)
+	# graph_dict is ex drug_dict.
+	opt.graph_dict = read_graph_structure(opt.drug_data)
 	opt.side_effects, opt.side_effect_idx_dict = read_ddi_instances(
 		opt.ddi_data, use_small_dataset=opt.debug)
 	opt.pos_datasets, opt.neg_datasets = prepare_dataset(opt.side_effects, opt.drug_dict)
@@ -88,6 +89,9 @@ def prepare_decagon_cv_files(opt):
 
 
 def prepare_qm9_cv_files(opt):
+	opt.graph_dict = read_graph_structure(opt.graph_data)
+	opt.n_atom_type = 5 # CHONF
+	opt.n_bond_type = 5 # single, double, triple, aromatic, self
 	return opt
 
 
@@ -97,7 +101,13 @@ def main():
 	                    nargs='+', choices=['qm9', 'decagon'],
 	                    help='Name of dataset to download [QM9,DECAGON]')
 
+	# I/O
+	parser.add_argument('-p', '--path', metavar='dir', type=str, nargs=1,
+						help="path to store the data (default ./data/)")
+
 	parser.add_argument('--ddi_data', default='bio-decagon-combo.csv')
+
+	#TODO: modify
 	parser.add_argument('--drug_data', default='drug.feat.wo_h.self_loop.idx.jsonl')
 	parser.add_argument('-n_fold', default=10, type=int)
 	parser.add_argument('--debug', action='store_true')
@@ -105,12 +115,15 @@ def main():
 
 	if "qm9" in opt.datasets:
 		opt = prepare_qm9_cv_files(opt)
+		file_name = opt.path + "qm9/" + "folds/" + opt.n_fold + "fold.npy"
 
 	if "decagon" in opt.datasets:
 		opt = prepare_decagon_cv_files(opt)
+		file_name = opt.path + "decagon/" + "folds/" + opt.n_fold + "fold.npy"
 
-	print('Dump to file:', opt.output_npy)
-	np.save(opt.output_npy, opt)
+	print('Dump to file:', file_name)
+	np.save(file_name, opt)
+
 
 if __name__ == '__main__':
 	main()

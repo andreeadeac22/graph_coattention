fd0042e71ee83f1cc821a20c6675cf8e7dfee837
andreeadeac22 <andreeadeac22@gmail.com>
2019-09-19 14:17:40 -0400

    Working fill in opt, without 10fold ddi

--- train.py
+++ train.py
@@ -1,35 +1,61 @@
 """
 	File used to train the networks.
 """
-import csv
 import os
 import pprint
-import time
 import random
 import logging
 import argparse
-from prettytable import PrettyTable
+import pickle
 
 import numpy as np
-from sklearn import metrics
-from tqdm import tqdm
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
 import torch.utils.data
+
+from utils.file_utils import setup_running_directories, save_experiment_settings
+
+def post_parse_args(opt):
+	# Set the random seed manually for reproducibility.
+	random.seed(opt.seed)
+	np.random.seed(opt.seed)
+	torch.manual_seed(opt.seed)
+	if torch.cuda.is_available():
+		torch.cuda.manual_seed(opt.seed)
+	if not hasattr(opt, 'exp_prefix'):
+		opt.exp_prefix = opt.memo + '-cv_{}_{}'.format(opt.fold_i, opt.n_fold)
+	if opt.debug:
+		opt.exp_prefix = 'dbg-{}'.format(opt.exp_prefix)
+	if not hasattr(opt, 'global_step'):
+		opt.global_step = 0
+
+	opt.best_model_pkl = os.path.join(opt.model_dir, opt.exp_prefix + '.pth')
+	opt.result_csv_file = os.path.join(opt.result_dir, opt.exp_prefix + '.csv')
+	return opt
 
 
 def main():
 	parser = argparse.ArgumentParser()
+	parser.add_argument('dataset', metavar='D', type=str.lower,
+	                    choices=['qm9', 'decagon'],
+	                    help='Name of dataset to used for training [QM9,DECAGON]')
 
 	# Directory to resume training from
 	parser.add_argument('--trained_setting_pkl', default=None,
 						help='Load trained model from setting pkl')
 
+	#parser.add_argument('input_data', default=None,
+	#                    help='Loading additional input data (n_atoms,...)'
+	#                        'e.g. ./data/decagon/folds/input_data.npy')
+
 	# Directory containing precomputed training data split.
-	parser.add_argument('--data_pkl', default=None,
-						help="Fold data path, e.g. ./data/decagon/folds/")
+	parser.add_argument('input_data_path', default=None,
+						help="Input data path, e.g. ./data/decagon/ "
+						     "or ./data/qm9/dsgdb9nsd/")
+
+	#parser.add_argument('--qm9_labels', default=
+	#	'./data/qm9/dsgdb9nsd/drug.labels.jsonl')
+	#parser.add_argument('--ddi_data', default=
+	#	'./data/decagon/bio-decagon-combo.csv')
 
 	parser.add_argument('-f', '--fold', default='1/10', type=str,
 	                    help="Which fold to run, format x/total")
@@ -55,7 +81,7 @@ def main():
 	parser.add_argument('-l2', '--l2_lambda', type=float, default=0)
 	parser.add_argument('-drop', '--dropout', type=float, default=0.1)
 	parser.add_argument('--patience', type=int, default=32)
-	parser.add_argument('--seed', type=int, default=777)
+	parser.add_argument('--seed', type=int, default=1337)
 	parser.add_argument('-nr', '--train_neg_pos_ratio', type=int, default=1)
 	parser.add_argument('-tr', '--transR', action='store_true')
 	parser.add_argument('-th', '--transH', action='store_true')
@@ -71,12 +97,56 @@ def main():
 		opt = np.load(open(opt.trained_setting_pkl, 'rb')).item()
 	else:
 		opt = post_parse_args(opt)
-		setup_directories(opt)
+		setup_running_directories(opt)
 		# save the dataset split in setting dictionary
 		pprint.pprint(vars(opt))
 		# save the setting
 		logging.info('Related data will be saved with prefix: %s', opt.exp_prefix)
 
+	assert os.path.exists(opt.input_data_path + "folds/")
+
+	if "qm9" in opt.dataset:
+		data_opt = np.load(open(opt.input_data_path + "input_data.npy",'rb')).item()
+
+		opt.n_atom_type = data_opt.n_atom_type
+		opt.n_bond_type = data_opt.n_bond_type
+
+
+		opt.train_graph_dict = pickle.load(open(opt.input_data_path + "folds/" + "train_graphs.npy", "rb"))
+		opt.train_labels_dict = pickle.load(open(opt.input_data_path + "folds/" + "train_labels.npy", "rb"))
+
+		opt.valid_graph_dict = pickle.load(open(opt.input_data_path + "folds/" + "valid_graphs.npy", "rb"))
+		opt.valid_labels_dict = pickle.load(open(opt.input_data_path + "folds/" + "valid_labels.npy", "rb"))
+
+		opt.test_graph_dict = pickle.load(open(opt.input_data_path + "folds/" + "test_graphs.npy", "rb"))
+		opt.test_labels_dict = pickle.load(open(opt.input_data_path + "folds/" + "test_labels.npy", "rb"))
+
+
+
+	if "decagon" in opt.dataset:
+		data_opt = np.load(open(opt.input_data_path + "input_data.npy", 'rb')).item()
+		opt.n_atom_type = data_opt.n_atom_type
+		opt.n_bond_type = data_opt.n_bond_type
+		opt.n_side_effect = data_opt.n_side_effect
+
+		graph_dict = pickle.load(open(opt.input_data_path + "folds/" + "1fold.npy", "rb"))
+		for se in graph_dict['pos']:
+			print(se)
+			print(graph_dict['pos'][se])
+			break
+
+		"""
+		assert data_opt.n_side_effect == len(opt.side_effects)
+
+		data_opt = np.load(open(opt.data_pkl, 'rb')).item()
+		opt.ddi_data = data_opt.ddi_data
+		opt.drug_data = data_opt.drug_data
+		opt.drug_dict = data_opt.drug_dict
+		opt.side_effects = data_opt.side_effects
+		opt.side_effect_idx_dict = data_opt.side_effect_idx_dict
+		"""
+
+	save_experiment_settings(opt)
 
 
 if __name__ == "__main__":
--- utils/file_utils.py
+++ utils/file_utils.py
@@ -1,6 +1,25 @@
 import os
+import logging
+import numpy as np
 
 # If not exists creates the specified folder
 def prepare_data_dir(path):
     if not os.path.exists(path):
         os.mkdir(path)
+
+
+def setup_running_directories(opt):
+	if not os.path.exists(opt.setting_dir):
+		os.makedirs(opt.setting_dir)
+
+	if not os.path.exists(opt.model_dir):
+		os.makedirs(opt.model_dir)
+
+	if not os.path.exists(opt.result_dir):
+		os.makedirs(opt.result_dir)
+
+
+def save_experiment_settings(opt):
+	setting_npy_path = os.path.join(opt.setting_dir, opt.exp_prefix + '.npy')
+	logging.info('Setting of the experiment is saved to %s', setting_npy_path)
+	np.save(setting_npy_path, opt)

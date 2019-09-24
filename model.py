8b40dc95a3212f8f29fe8a6995e8f26525447978
Louis-Pascal Xhonneux <lpxhonneux@gmail.com>
2019-09-24 19:10:19 -0400

    Add pairing function (tested for first batch)

--- model.py
+++ model.py
@@ -50,6 +50,7 @@ class DrugDrugInteractionNetwork(nn.Module):
 
 		self.__score_fn = score_fn
 		self.pair_model = GraphPairNN(batch_size, batch_size, batch_size).cuda()
+		self.batch_size = batch_size
 
 	@property
 	def score_fn(self):
@@ -64,22 +65,81 @@ class DrugDrugInteractionNetwork(nn.Module):
 			se_idx=None, drug_se_seg=None):
 
 		# generate input indices + padding
-		ind_in =np.arange(0, -(-len(seg_m1) // self.pair_model.out_features) * self.pair_model.out_features)
+		ind_in =np.arange(0, self.batch_size)
 		ind_in = torch.from_numpy(ind_in).type(torch.FloatTensor)
-		ind_in = ind_in.reshape(shape=(ind_in.shape[0] // self.pair_model.out_features, self.pair_model.out_features))
+		ind_in = ind_in.reshape(shape=(1, self.batch_size))
 
 		ind_out = self.pair_model(ind_in.cuda())
 		ind_out = ind_out.reshape(shape=(ind_in.shape[0] * ind_in.shape[1], ind_in.shape[1]))
 		preds = torch.max(ind_out, dim=1)[1].type(torch.LongTensor)
 
-		seg_m2 = seg_m2[preds]
-		atom_type2 = atom_type2[preds]
-		atom_feat2 = atom_feat2[preds]
-		bond_type2 = bond_type2[preds]
-		inn_seg_i2 = inn_seg_i2[preds]
-		inn_idx_j2 = inn_idx_j2[preds]
-		out_seg_i2 = out_seg_i2[preds]
-		out_idx_j2 = out_idx_j2[preds]
+		atom_batch_dim, n_atom1 = torch.unique(seg_m1, return_counts=True)
+		c_atom1 = n_atom1.cumsum(dim=0)-n_atom1[0]
+		n_atom1_perm = n_atom1[preds]
+		c_atom1_perm = n_atom1_perm.cumsum(dim=0)
+		atom_batch_dim, n_atom2 = torch.unique(seg_m2, return_counts=True)
+		bond_batch_dim = torch.zeros(inn_seg_i2.shape[0])-1
+		bond_batch_dim = bond_batch_dim.type(torch.int).cuda()
+		c_atom2 = torch.zeros(n_atom2.shape[0]+1).cuda()
+		c_atom2[1:] = n_atom2.cumsum(dim=0)
+		c_atom2 = c_atom2.type(torch.LongTensor).cuda()
+		n_atom2_perm = n_atom2[preds]
+		c_atom2_perm = n_atom2_perm.cumsum(dim=0)
+		seg_m2_perm = torch.repeat_interleave(atom_batch_dim, n_atom2_perm)
+		atom_shuffle = torch.repeat_interleave(atom_batch_dim * c_atom2_perm, n_atom2_perm)
+		cum_c_atom = 0
+		for i in range(self.batch_size):
+			atom_shuffle[cum_c_atom:cum_c_atom+n_atom2_perm[i]] = c_atom2[preds[i]]+ torch.arange(n_atom2_perm[
+																									   i]).cuda()
+			cum_c_atom += n_atom2_perm[i]
+		atom_type2_perm = atom_type2[atom_shuffle]
+		atom_feat2_perm = atom_feat2[atom_shuffle, :]
+
+		# finding batch_dimension of bonds
+		for i in range(self.batch_size):
+			bond_batch_dim += 1*inn_seg_i2.ge(c_atom2[i]).int().cuda() + 0*inn_seg_i2.lt(c_atom2[i]).int().cuda()
+
+
+		_ , n_bond2 = torch.unique(bond_batch_dim, return_counts=True)
+		c_bond2 = torch.zeros(n_atom2.shape[0] + 1).cuda()
+		c_bond2[1:] = n_bond2.cumsum(dim=0)
+		c_bond2 = c_bond2.type(torch.LongTensor).cuda()
+		bond_seg2 = inn_seg_i2 - torch.repeat_interleave(c_atom2[:-1], n_bond2)
+		bond_idx2 = inn_idx_j2 - torch.repeat_interleave(c_atom2[:-1], n_bond2)
+
+		# construct bond shuffle
+		n_bond2_perm = n_bond2[preds]
+		c_bond2_perm = torch.zeros(n_atom2.shape[0] + 1).cuda()
+		c_bond2_perm[1:] = n_bond2_perm.cumsum(dim=0)
+		c_bond2_perm = c_bond2_perm.type(torch.LongTensor).cuda()
+		bond_shuffle = torch.zeros(c_bond2_perm[128])
+		cum_c_bond = 0
+		for i in range(self.batch_size):
+			bond_shuffle[cum_c_bond:cum_c_bond + n_bond2_perm[i]] = c_bond2[preds[i]] + torch.arange(n_bond2_perm[
+																										  i]).cuda()
+			cum_c_bond += n_bond2_perm[i]
+		bond_shuffle = bond_shuffle.type(torch.LongTensor).cuda()
+		bond_type2 = bond_type2[bond_shuffle]
+		inn_idx_j2 = bond_idx2[bond_shuffle] + torch.repeat_interleave(c_atom2_perm-c_atom2_perm[0], n_bond2_perm)
+		inn_seg_i2 = bond_seg2[bond_shuffle] + torch.repeat_interleave(c_atom2_perm-c_atom2_perm[0], n_bond2_perm)
+
+		# collating drug pairs again
+		ddi_seg_i1, ddi_seg_i2, ddi_idx_j1, ddi_idx_j2 = zip(*[
+			(i1 + c1, i2 + c2, i2, i1)
+			for l1, l2, c1, c2 in zip(n_atom1, n_atom2_perm, c_atom1, c_atom2_perm-n_atom2_perm[0])
+			for i1 in range(l1) for i2 in range(l2)])
+
+		seg_m2 = seg_m2_perm.type(torch.LongTensor).cuda()
+		atom_type2 = atom_type2_perm.type(torch.LongTensor).cuda()
+		atom_feat2 = atom_feat2_perm.type(torch.FloatTensor).cuda()
+		bond_type2 = bond_type2.type(torch.LongTensor).cuda()
+		inn_seg_i2 = inn_seg_i2.type(torch.LongTensor).cuda()
+		inn_idx_j2 = inn_idx_j2.type(torch.LongTensor).cuda()
+
+		out_seg_i1 = torch.LongTensor(ddi_seg_i1).type(torch.LongTensor).cuda()
+		out_idx_j1 = torch.LongTensor(ddi_idx_j1).type(torch.LongTensor).cuda()
+		out_seg_i2 = torch.LongTensor(ddi_seg_i2).type(torch.LongTensor).cuda()
+		out_idx_j2 = torch.LongTensor(ddi_idx_j2).type(torch.LongTensor).cuda()
 
 		atom1 = self.dropout(self.atom_comp(atom_feat1, atom_type1))
 		atom2 = self.dropout(self.atom_comp(atom_feat2, atom_type2))

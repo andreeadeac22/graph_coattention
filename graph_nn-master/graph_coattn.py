1fbb50eb7ec7716416cb8261bf5a49028eba9650
andreeadeac22 <andreeadeac22@gmail.com>
2019-11-07 02:24:58 -0500

    Rewriting co-attn for new data pipeline)

--- graph_nn-master/graph_coattn.py
+++ graph_nn-master/graph_coattn.py
@@ -3,27 +3,6 @@ import torch
 import torch.nn as nn
 
 
-class RadialBasisFunctionExpansion(nn.Module):
-	def __init__(self, n_bin, low=0, high=20):
-		super().__init__()
-		assert high > low
-
-		self.n_center = n_bin
-		self.gap = (high - low) / n_bin
-		self.centers = nn.Parameter(
-			torch.FloatTensor(np.linspace(low, high, n_bin)).view(1, -1))
-
-	def forward(self, dist):
-		'''
-		input:  dist:   [(b*n_v*n_v) x 1]
-		output: rbf:    [(b*n_v*n_v) x n_bin]
-		'''
-
-		rbf = dist.unsqueeze(-1) - self.centers  # [(b*n_v*n_v) x n_centers]
-		rbf = torch.exp(-(rbf.pow(2)) / self.gap)
-		return rbf
-
-
 def segment_max(logit, n_seg, seg_i, idx_j):
 	max_seg_numel = idx_j.max().item() + 1
 	seg_max = logit.new_full((n_seg, max_seg_numel), -np.inf)
@@ -51,7 +30,11 @@ def segment_multihead_expand(seg_i, n_seg, n_head):
 
 
 class CoAttention(nn.Module):
-	def __init__(self, d_in, d_out, n_head=1, dropout=0.1):
+	def __init__(self,
+			d_in,
+			d_out,
+			n_head=1,
+			dropout=0.1):
 		super().__init__()
 		self.temperature = np.sqrt(d_in)
 
@@ -63,71 +46,40 @@ class CoAttention(nn.Module):
 		nn.init.xavier_normal_(self.key_proj.weight)
 		nn.init.xavier_normal_(self.val_proj.weight)
 
+		self.softmax = nn.Softmax(dim=1)
+
 		self.attn_drop = nn.Dropout(p=dropout)
 		self.out_proj = nn.Sequential(
 			nn.Linear(d_in * n_head, d_out),
 			nn.LeakyReLU(), nn.Dropout(p=dropout))
 
-	def forward(self, node1, seg_i1, idx_j1, node2, seg_i2, idx_j2, entropy=[]):
 
-		d_h = node1.size(1)
+	def forward(self, x1, x2, entropy=[]):
 
-		n_seg1 = node1.size(0)
-		n_seg2 = node2.size(0)
-
+		# Why is there a key_proj and a val_proj?
 		# Copy center for attention key
-		node1_ctr = self.key_proj(node1).index_select(0, seg_i1)
-		node2_ctr = self.key_proj(node2).index_select(0, seg_i2)
-
+		#node1_ctr = self.key_proj(node1).index_select(0, seg_i1)
+		#node2_ctr = self.key_proj(node2).index_select(0, seg_i2)
 		# Copy neighbor for attention value
-		node1_nbr = self.val_proj(node2).index_select(0, seg_i2)  # idx_j1 == seg_i2
-		node2_nbr = self.val_proj(node1).index_select(0, seg_i1)  # idx_j2 == seg_i1
+		#node1_nbr = self.val_proj(node2).index_select(0, seg_i2)  # idx_j1 == seg_i2
+		#node2_nbr = self.val_proj(node1).index_select(0, seg_i1)  # idx_j2 == seg_i1
 
-		arg_i1 = None
-		arg_i2 = None
+		h1 = self.key_proj(x1)
+		h2 = self.key_proj(x2)
 
-		if self.multi_head:
-			# prepare copied and shifted index tensors
-			seg_i1 = segment_multihead_expand(seg_i1, n_seg1, self.n_head)
-			seg_i2 = segment_multihead_expand(seg_i2, n_seg2, self.n_head)
+		e12 = torch.bmm(h1, torch.transpose(h2, -1, -2))
+		e21 = torch.bmm(h2, torch.transpose(h1, -1, -2))
 
-			idx_j1 = idx_j1.unsqueeze(1).expand(-1, self.n_head).contiguous().view(-1)
-			idx_j2 = idx_j2.unsqueeze(1).expand(-1, self.n_head).contiguous().view(-1)
+		# what is segment_softmax
+		# dropout?
+		a12 = self.softmax(e12)
+		a21 = self.softmax(e21)
 
-			# prepare for the final multi-head concatenation
-			arg_i1 = segment_multihead_expand(
-				seg_i1.new_tensor(np.arange(n_seg1)), n_seg1, self.n_head)
-			arg_i2 = segment_multihead_expand(
-				seg_i2.new_tensor(np.arange(n_seg2)), n_seg2, self.n_head)
-
-			# pile up as regular input
-			node1_ctr = node1_ctr.view(-1, d_h)
-			node2_ctr = node2_ctr.view(-1, d_h)
-
-			node1_nbr = node1_nbr.view(-1, d_h)
-			node2_nbr = node2_nbr.view(-1, d_h)
-
-			# new numbers of segments
-			n_seg1 = n_seg1 * self.n_head
-			n_seg2 = n_seg2 * self.n_head
-
-		translation = (node1_ctr * node2_ctr).sum(1)
+		n12 = torch.bmm(a12, self.val_proj(x2))
+		n21 = torch.bmm(a21, self.val_proj(x1))
 
 		# TODO!! Remove this while training, this is just for entropy.
 		#translation = torch.ones_like(translation)
-
-		# Calculate attention weight as edges between two graphs
-		node1_edge = self.attn_drop(segment_softmax(
-			translation, n_seg1, seg_i1, idx_j1, self.temperature))
-		node2_edge = self.attn_drop(segment_softmax(
-			translation, n_seg2, seg_i2, idx_j2, self.temperature))
-
-		node1_edge = node1_edge.view(-1, 1)
-		node2_edge = node2_edge.view(-1, 1)
-
-		# Weighted sum
-		msg1 = node1.new_zeros((n_seg1, d_h)).index_add(0, seg_i1, node1_edge * node1_nbr)
-		msg2 = node2.new_zeros((n_seg2, d_h)).index_add(0, seg_i2, node2_edge * node2_nbr)
 
 		# Entropy computation
 		#ent1 = node1.new_zeros((n_seg1, 1)).index_add(0, seg_i1, torch.sum(node1_edge * torch.log(node1_edge + 1e-7), -1))
@@ -135,13 +87,10 @@ class CoAttention(nn.Module):
 		#entropy.append(ent1)
 		#entropy.append(ent2)
 
-		if self.multi_head:
-			msg1 = msg1[arg_i1].view(-1, d_h * self.n_head)
-			msg2 = msg2[arg_i2].view(-1, d_h * self.n_head)
+		msg1 = self.out_proj(n12)
+		msg2 = self.out_proj(n21)
 
-		msg1 = self.out_proj(msg1)
-		msg2 = self.out_proj(msg2)
-
+		#Check if 1 -> 1 and 2 -> 2.
 		return msg1, msg2
 
 
@@ -164,8 +113,7 @@ class MessagePassing(nn.Module):
         A_hat = A + I
         return A_hat
 
-	def forward(self, data):
-        x, A, mask = data[:3]
+	def forward(self, x, A, mask):
         # print('in', x.shape, torch.sum(torch.abs(torch.sum(x, 2)) > 0))
         if len(A.shape) == 3:
             A = A.unsqueeze(3)
@@ -218,22 +166,34 @@ class CoAttentionMessagePassingNetwork(nn.Module):
 
 	def forward(
 			self,
-			seg_g1, node1, edge1, inn_seg_i1, inn_idx_j1, out_seg_i1, out_idx_j1,
-			seg_g2, node2, edge2, inn_seg_i2, inn_idx_j2, out_seg_i2, out_idx_j2,
-						entropies=[]):
+			data,
+			entropies=[]):
+
+		x, A, masks = data[:3]
+
+		evens = torch.tensor([i for i in range(0,x.shape[0],2)])
+		odds = torch.tensor([i for i in range(1,x.shape[0],2)])
+
+		x1 = torch.index_select(x, 0, evens)
+		A1 = torch.index_select(A, 0, evens)
+		masks1 = torch.index_select(masks, 0, evens)
+
+		x2 = torch.index_select(x, 0, odds)
+		A2 = torch.index_select(A, 0, odds)
+		masks2 = torch.index_select(masks, 0, odds)
 
 		for step_i in range(self.n_prop_step):
 			#if step_i >= len(entropies):
 			#	entropies.append([])
-			inner_msg1 = self.mps[step_i](node1, edge1, inn_seg_i1, inn_idx_j1)
-			inner_msg2 = self.mps[step_i](node2, edge2, inn_seg_i2, inn_idx_j2)
+			inner_msg1 = self.mps[step_i](x1, A1, masks1)
+			inner_msg2 = self.mps[step_i](x2, A2, masks2)
 			outer_msg1, outer_msg2 = self.coats[step_i](
-				node1, out_seg_i1, out_idx_j1,
-				node2, out_seg_i2, out_idx_j2, [])
+				x1,
+				x2, [])
 				# node2, out_seg_i2, out_idx_j2, entropies[step_i])
 
-			node1 = self.lns[step_i](self.update_fn(node1, inner_msg1, outer_msg1))
-			node2 = self.lns[step_i](self.update_fn(node2, inner_msg2, outer_msg2))
+			node1 = self.lns[step_i](self.update_fn(x1, inner_msg1, outer_msg1))
+			node2 = self.lns[step_i](self.update_fn(x2, inner_msg2, outer_msg2))
 
 		g1_vec = self.readout(node1, seg_g1)
 		g2_vec = self.readout(node2, seg_g2)
